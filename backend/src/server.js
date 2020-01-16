f9079ea3b5079da1bbf77c3637bc1d9679167b16
Luis Emanuel Alderete <luisemanuel.alderete@gmail.com>
2020-01-16 11:13:20 -0200

    Implementação do CORS, para testes com ReactJS

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const database = require('./config/database');
 const routes = require('./routes');
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
